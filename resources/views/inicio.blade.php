16cc4a1ccf66479c4c2a414794a4d9328e2d3d6c
DAW <juanmiguel.valle@iesdonana.org>
2025-04-21 16:33:32 +0200

    migraciones

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+    @vite('resources/css/app.css') {{-- Si estás usando Vite y Tailwind --}}
+</head>
+
+<body class="bg-gray-100 text-gray-800 ">
+    <div class="flex justify-center items-center h-screen">
+
+        <button onclick="window.location='{{ route('compra') }}'" type="button"
+        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-5xl px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-[500px] h-[300px]">Iniciar
+        compra</button>
+    </div>
+        
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('inicio');
+})->name('inicio');
+
+Route::get('/compra', function () {
+    return view('compra');
+})->name('compra');
